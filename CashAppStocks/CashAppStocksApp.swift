3af8d71042d5f786d93aacbdd900278687c23e0a
Syed Raza <syed.raza011@gmail.com>
2023-07-10 11:14:01 -0400

    Initial Commit

---
+++ CashAppStocks/CashAppStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashAppStocksApp.swift
+//  CashAppStocks
+//
+//  Created by Syed Raza on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CashAppStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
