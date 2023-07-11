aa45daad568611dcf904a7d88b75b43c5c0cd1f5
Syed Raza <syed.raza011@gmail.com>
2023-07-10 20:49:10 -0400

    search and details added

--- CashAppStocks/ContentView.swift
+++ CashAppStocks/ContentView.swift
@@ -7,43 +7,93 @@
 
 import SwiftUI
 import Combine
+
+
 struct ContentView: View {
     @StateObject var viewModel = StockViewModel()
+    @State private var searchText = ""
     
     var body: some View {
         NavigationView {
-          
+            VStack {
                 if viewModel.stocks.isEmpty {
                     ProgressView()
                 } else {
-                    List(viewModel.stocks) { stock in
-                        NavigationLink(destination: StockDetailsView()) {
-                            VStack {
-                                Text(stock.ticker)
-                                Text(stock.currency)
-                                Text(stock.name)
-                                Text(String(stock.current_price_cents))
-                                Text(String(stock.current_price_timestamp))
+                    ScrollView {
+                        LazyVGrid(columns: [GridItem(.adaptive(minimum: 200), spacing: 16)], spacing: 16) {
+                            ForEach(filteredStocks) { stock in
+                                NavigationLink(destination: StockDetailsView(stock: stock)) {
+                                    VStack(alignment: .leading, spacing: 10) {
+                                        Text(stock.name)
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                            .padding(.horizontal)
+                                        Text(formatPriceCents(stock.current_price_cents))
+                                            .font(.subheadline)
+                                            .foregroundColor(.white)
+                                            .padding(.horizontal)
+                                        Text(formatTimestamp(stock.current_price_timestamp))
+                                            .font(.subheadline)
+                                            .foregroundColor(.white)
+                                            .padding(.horizontal)
+                                    }
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .cornerRadius(10)
+                                }
+                                .buttonStyle(PlainButtonStyle()) // Remove default button style
                             }
                         }
+                        .padding()
                     }
                 }
-            
-           
-        } .navigationTitle("Stocks")
+            }
+            .navigationTitle("New York Stocks")
+            .searchable(text: $searchText, prompt: "Search")
+            .navigationBarItems(trailing: homeButton)
+        }
         .onAppear {
             viewModel.getStocks()
         }
     }
-}
-
-
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    var homeButton: some View {
+        Button(action: {
+            // Handle home button action
+        }) {
+            Image(systemName: "house")
+                .foregroundColor(.white)
+        }
+    }
+    
+    var filteredStocks: [Stock] {
+        if searchText.isEmpty {
+            return viewModel.stocks
+        } else {
+            return viewModel.stocks.filter { stock in
+                stock.name.localizedCaseInsensitiveContains(searchText) ||
+                    stock.currency.localizedCaseInsensitiveContains(searchText) ||
+                    formatPriceCents(stock.current_price_cents).localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
+    
+    func formatPriceCents(_ priceCents: Int) -> String {
+        let formattedPrice = String(format: "$%.2f", Double(priceCents) / 100)
+        return formattedPrice
+    }
+    
+    func formatTimestamp(_ timestamp: Int) -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let formattedDate = dateFormatter.string(from: date)
+        return formattedDate
     }
 }
+
+
+
+
 
---
+++ CashAppStocks/Network/TimeFixture.swift
@@ -0,0 +1,21 @@
+//
+//  TimeFixture.swift
+//  CashAppStocks
+//
+//  Created by Syed Raza on 7/10/23.
+//
+
+import Foundation
+import Combine
+
+func formatTimestamp(_ timestamp: Int) -> String {
+    let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    let formattedDate = dateFormatter.string(from: date)
+    return formattedDate
+}
+func formatPriceCents(_ priceCents: Int) -> String {
+    let formattedPrice = String(format: "$%.2f", Double(priceCents) / 100)
+    return formattedPrice
+}
