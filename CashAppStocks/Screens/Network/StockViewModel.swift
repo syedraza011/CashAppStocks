5fd274f11b0a0e1d93556da315da6f570cc0e850
Syed Raza <syed.raza011@gmail.com>
2023-07-20 21:56:46 -0400

    minor updates

--- CashAppStocks/Screens/Network/StockViewModel.swift
+++ CashAppStocks/Screens/Network/StockViewModel.swift
@@ -6,48 +6,6 @@
 //
 
 
-//import Foundation
-//import Combine
-
-//class StocksViewModel: ObservableObject {
-//
-//    @Published var fullStocks = StockResponse(stocks: [Stock]())
-//    @Published var relatedItems: [Stock] = []
-//    @Published var state: AsyncState = .initial
-//    let service: StocksServiceProtocol
-//
-//    init(service: StocksServiceProtocol = StocksService()) {
-//        self.service = service
-//    }
-//
-//    @MainActor func getStocks() {
-//        state = .loading
-//        Task {
-//            do {
-//                // For fetching and then reformatting the stocks, throw errors here or elsewhere btw
-//                let myStocks: StockResponse = try await self.service.fetchStocks()
-//                if (myStocks.stocks.isEmpty) {throw APIError.emptyData}
-//                self.fullStocks = myStocks
-//                state = .loaded
-//
-//            } catch {
-//                if let error = error as? APIError {
-//                    print(error.description)
-//                    state = .error
-//                    throw error
-//
-//                } else {
-//                    print(error)
-//                    state = .error
-//                    throw error
-//
-//                }
-//            }
-//        }
-//    }
-//
-//}
-
 import Foundation
 import Combine
 
